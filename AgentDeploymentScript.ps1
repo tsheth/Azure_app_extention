56015d10c923e99cc73ed7ada588fa9b2ab11d6c
Tejas <tsheth.p@gmail.com>
2018-08-16 22:39:43 +0530

    Windows script

---
+++ AgentDeploymentScript.ps1
@@ -0,0 +1,49 @@
+
+#requires -version 4.0
+
+# PowerShell 4 or up is required to run this script
+# This script detects platform and architecture.  It then downloads and installs the relevant Deep Security Agent package
+
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+   Write-Warning "You are not running as an Administrator. Please try again with admin privileges."
+   exit 1
+}
+
+$env:LogPath = "$env:appdata\Trend Micro\Deep Security Agent\installer"
+New-Item -path $env:LogPath -type directory
+Start-Transcript -path "$env:LogPath\dsa_deploy.log" -append
+
+echo "$(Get-Date -format T) - DSA download started"
+$baseUrl="https://app.deepsecurity.trendmicro.com:443/"
+if ( [intptr]::Size -eq 8 ) { 
+   $sourceUrl=-join($baseurl, "software/agent/Windows/x86_64/") }
+else {
+   $sourceUrl=-join($baseurl, "software/agent/Windows/i386/") }
+echo "$(Get-Date -format T) - Download Deep Security Agent Package" $sourceUrl
+
+$WebClient = New-Object System.Net.WebClient
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+
+Try
+{
+     $WebClient.DownloadFile($sourceUrl,  "$env:temp\agent.msi")
+} Catch [System.Net.WebException]
+{
+      echo " Please check that your Deep Security Manager TLS certificate is signed by a trusted root certificate authority."
+      exit 2;
+}
+
+if ( (Get-Item "$env:temp\agent.msi").length -eq 0 ) {
+    echo "Failed to download the Deep Security Agent. Please check if the package is imported into the Deep Security Manager. "
+ exit 1
+}
+echo "$(Get-Date -format T) - Downloaded File Size:" (Get-Item "$env:temp\agent.msi").length
+
+echo "$(Get-Date -format T) - DSA install started"
+echo "$(Get-Date -format T) - Installer Exit Code:" (Start-Process -FilePath msiexec -ArgumentList "/i $env:temp\agent.msi /qn ADDLOCAL=ALL /l*v `"$env:LogPath\dsa_install.log`"" -Wait -PassThru).ExitCode 
+echo "$(Get-Date -format T) - DSA activation started"
+Start-Sleep -s 50
+& $Env:ProgramFiles"\Trend Micro\Deep Security Agent\dsa_control" -r
+& $Env:ProgramFiles"\Trend Micro\Deep Security Agent\dsa_control" -a dsm://agents.deepsecurity.trendmicro.com:443/ "tenantID:7EC252D6-8B8D-62B3-4D23-C90766391435" "token:04763BE2-1DFC-1C65-EE91-AC23ABF70318" "policyid:203"
+Stop-Transcript
+echo "$(Get-Date -format T) - DSA Deployment Finished"
